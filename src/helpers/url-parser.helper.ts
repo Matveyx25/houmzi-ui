88c312e02a6958310e26370d112d5d9e19109aff
Matveyx25 <matveyx25@gmail.com>
2023-05-02 15:40:00 +0400

    url translator

--- src/helpers/url-parser.helper.ts
+++ src/helpers/url-parser.helper.ts
@@ -1,6 +1,22 @@
-export const stringToUrl = (str: string): string => str
-  .replace(/[,./]/g, '')
-  .replace(/ /g, '-')
+import { translitRuEn } from "./transliter"
+
+const removeSmallWord = (str, limit = 3) => {
+  let arr = str.split(" "); 
+  let newArr = [];
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i].length >= limit) {
+      newArr.push(arr[i]);
+    }
+  }
+  let newStr = newArr.join(" ");
+  return newStr; 
+}
+
+
+export const stringToUrl = (str: string): string => {
+  const res = removeSmallWord(str.toLowerCase()).replace(/[,./]/g, '').replace(/ /g, '-')
+  return translitRuEn(res)
+}
 
 export const urlToString = (url: string): string => url
   .replace(/-/g, ' ')
